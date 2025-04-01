84cbf7c94f4f48e24f8197bd18958aa9d661e60e
0x6A6471 <hello@jakequinter.io>
2025-04-01 18:42:20 -0500

    feat: switch

--- src/switch.ml
+++ src/switch.ml
@@ -1,29 +1,23 @@
 open React
 
-type data_state =
-  [ `checked
-  | `unchecked
-  ]
+module Root = struct
+  external make
+    :  ?asChild:bool
+    -> ?defaultChecked:bool
+    -> checked:bool
+    -> onCheckedChange:((bool -> bool) -> unit)
+    -> ?disabled:bool
+    -> ?required:bool
+    -> ?name:string
+    -> ?value:string
+    -> ?className:string
+    -> ?children:element
+    -> element
+    = "Root"
+  [@@react.component] [@@mel.module "@radix-ui/react-switch"]
+end
 
-external root
-  :  ?asChild:bool
-  -> ?defaultChecked:bool
-  -> checked:bool
-  -> onCheckedChange:(bool -> unit)
-  -> ?disabled:bool
-  -> ?required:bool
-  -> ?name:string
-  -> ?value:string
-  -> ?dataState:(data_state[@mel.as "data-state"])
-  -> ?dataDisabled:(bool[@mel.as "data-disabled"])
-  -> element
-  = "Root"
-[@@mel.module "@radix-ui/react-switch"]
-
-external thumb
-  :  ?asChild:bool
-  -> ?dataState:(data_state[@mel.as "data-state"])
-  -> ?dataDisabled:(bool[@mel.as "data-disabled"])
-  -> element
-  = "Thumb"
-[@@mel.module "@radix-ui/react-switch"]
+module Thumb = struct
+  external make : ?asChild:bool -> ?className:string -> element = "Thumb"
+  [@@react.component] [@@mel.module "@radix-ui/react-switch"]
+end
